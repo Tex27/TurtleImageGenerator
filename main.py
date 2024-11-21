085503477cf3b64f37349a9276f6f979e566c42f
Tex27 <75863550+Tex27@users.noreply.github.com>
2024-11-21 15:31:53 +0000

    small update
    Added loading,
    Image folder
    Auto Image height and with

--- main.py
+++ main.py
@@ -1,49 +1,76 @@
+import os
 from PIL import Image
 import turtle
+import time
 
-s = int(input("Tamanho da imagem em px: "))
-n = input("Nome da imagem: ")
-o = input("Opções do desenho \n r - print rápida \n n - normal \n >")
+n = input("Nome da imagem: \n > ")
+o = input("Opções do desenho \n r - print rápida \n n - normal \n > ")
 
-img = Image.open(f"{n}.png")
+img = Image.open(f"/media/sf_Document/GitHub/TurtleImageGenerator/imagens/{n}.png")
 img = img.convert("RGBA")
+l, a = img.size
+
+msg = "\nAnalisando imagem"
+for i in range(2):
+    print(f"{msg} /")
+    time.sleep(1)
+    os.system("clear")
+    print(f"{msg} -")
+    time.sleep(1)
+    os.system("clear")
+    print(f"{msg} \\")
+    time.sleep(1)
+    os.system("clear")
+    print(f"{msg} |")
+    time.sleep(1)
+    os.system("clear")
+
+time.sleep(1)
+print(f"Tamanho da imagem: {l}x{a}")
+time.sleep(2.5)
 
 if o == "n":
     turtle.shape("classic")
-    turtle.speed("fastest")
+    turtle.speed(0)
     turtle.penup()
-    for x in range(0, s):
-        for y in range(0, s):
+    sum = 0
+    for x in range(0, l):
+        for y in range(0, a):
             px = img.getpixel((x, y))
             color = '#%02x%02x%02x' % (px[0], px[1], px[2])
             turtle.setpos(x - 112, 112 - y)
             turtle.pendown()
             turtle.color(color)
             turtle.begin_fill()
-            turtle.dot(5)
+            turtle.dot(1)
             turtle.end_fill()
             turtle.penup()
-            print(f"{x}x{y}y: {px}")
-
-        turtle.done()
+            sum += 1
+            print(f"{sum}º px, ({x}x{y}y): {px}")
+            
+    print("\nA tua bela obra de arte está feita!")
+    turtle.done()
 
 elif o == "r":
     turtle.shape("classic")
-    turtle.speed("fastest")
+    turtle.speed(0)
     turtle.penup()
+    sum = 0
     turtle.tracer(0, 0)
-    for x in range(0, s):
-        for y in range(0, s):
+    for x in range(0, l):
+        for y in range(0, a):
             px = img.getpixel((x, y))
             color = '#%02x%02x%02x' % (px[0], px[1], px[2])
             turtle.setpos(x - 112, 112 - y)
             turtle.pendown()
             turtle.color(color)
             turtle.begin_fill()
-            turtle.dot(5)
+            turtle.dot(1)
             turtle.end_fill()
             turtle.penup()
-            print(f"({x}x, {y}y): {px}")
-
+            sum += 1
+            print(f"{sum}º px, ({x}x{y}y): {px}")
+    
+    print("\nA tua bela obra de arte está feita!")
     turtle.update()
     turtle.done()
